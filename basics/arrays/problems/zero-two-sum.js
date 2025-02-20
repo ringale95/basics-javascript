591acd9788c388b01639b2451f46c10614991e4d
Raveena Ingale <ingale.r@northeastern.edu>
2025-02-20 14:00:25 -0500

    added more js problems

---
+++ basics/arrays/problems/zero-two-sum.js
@@ -0,0 +1,14 @@
+// const getZeroTwoSum = (arr) => {
+//     let result = [];
+//     const map = {};
+//     arr.forEach((e, i) => {
+//         if (map[-e] != undefined)
+//             result = [map[-e], i];
+//         map[e] = i;
+//     })
+//     return result;
+// }
+
+// console.log(getZeroTwoSum([0, -1, -2, 2, 3]));
+
+console.log('a');
